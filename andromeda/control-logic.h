2157625c0f4ded96b5c4aecbdd21100863f8bbf8
Michele Ippolito <m.ippolito90@gmail.com>
2024-10-16 22:42:38 +0200

    Adds fade in/out for the effects
    Rework the control logic so that the brightness has linear brightness
    ramps to fade in and out the effects.
    
    Close #20

--- andromeda/control-logic.h
+++ andromeda/control-logic.h
@@ -11,13 +11,57 @@
 AbstractAnimation* animation;
 AbstractEffect*    effect;
 
-milliseconds MIN_EFFECT_DURATION =  60000;
-milliseconds MAX_EFFECT_DURATION = 240000;
-milliseconds nextTransition = 0;
+// These parameters control how long an effect lasts and how quickly it fades in and out
+milliseconds FADE_IN_DURATION  = 2500;
+milliseconds FADE_OUT_DURATION = 5000;
+milliseconds MIN_EFFECT_DURATION = 1 * (60*1000);
+milliseconds MAX_EFFECT_DURATION = 5 * (60*1000);
 
+// These contain the actual timestamps to plan the fade in/out and the transition to the next effect
+milliseconds effectStart = 0;       // time when the current effect started
+milliseconds nextTransition = 0;    // time when the current effect will end
+milliseconds fadeInEnd;             // time when the fade in will end ( = effectStart + FADE_IN_DURATION)
+milliseconds fadeOutStart;          // time when the fade out will start ( = nextTransition - FADE_OUT_DURATION)
+
+
+// Picks a new transition time and resets the other timestamps accordingly
 void setNextTransition()
 {
-  nextTransition = millis() + random(MIN_EFFECT_DURATION, MAX_EFFECT_DURATION);
+  effectStart = millis();
+  fadeInEnd = effectStart + FADE_IN_DURATION;
+  fadeOutStart = fadeInEnd + random(MIN_EFFECT_DURATION, MAX_EFFECT_DURATION);
+  nextTransition = fadeOutStart + FADE_OUT_DURATION;
+}
+
+// Return the master brightness to fade the effects in and out
+//
+//   fadeInEnd   fadeOutStart
+//     /¯¯¯¯¯¯¯¯¯¯¯¯¯¯\
+//    /                \
+// effectStart      nextTransition
+//
+byte getBrightness(milliseconds t)
+{
+  byte brightness = 0;
+
+  // Most of the time is spent in the middle so test that first
+  // we are all about microseconds in this highly efficient architecture
+  if (t >= fadeInEnd && t <= fadeOutStart)
+  {
+    brightness = 255;
+  }
+  else if (t < fadeInEnd)
+  {
+    milliseconds dt = (t - effectStart);
+    brightness = map(dt, 0, FADE_IN_DURATION, 0, 255);
+  }
+  else if (t > fadeOutStart)
+  {
+    milliseconds dt = (t - fadeOutStart);
+    brightness = map(dt, 0, FADE_OUT_DURATION, 255, 0);
+  }
+
+  return constrain(brightness, 0, 255);
 }
 
 // Pick a new random animation, play it, and deallocate it
@@ -25,14 +69,25 @@ void runRandomAnimation()
 {
     animation = getRandomAnimation();
 
+    // First fade everything out to black and add a small delay
+    // to create some separation from the effect
+    FastLED.setBrightness(0);
+    delay(200);
+
+    // Reset the brightness to max and give control back to the animation
+    FastLED.setBrightness(255);
     animation->run();
     animation->cleanup();
+
+    // Turn it down to zero and wait a little bit.
+    // This creates another small separation before the effect.
+    // The brightness must start from zero to avoid ugly jumps when the lerp kicks in
+    FastLED.setBrightness(0);
     delay(200);
 
     delete animation;
     animation = NULL;
 }
-
 
 // When the transition time is reached:
 // - play an animation
@@ -50,7 +105,12 @@ void handleTransition()
 void update(milliseconds t)
 {
   if (t >= nextTransition)
+  {
     handleTransition();
+    return;
+  }
+
+  FastLED.setBrightness(getBrightness(t));
 
   effect->precompute(t);
   effect->render(STRIPS, t);
