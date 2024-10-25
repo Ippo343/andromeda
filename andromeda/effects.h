517bfc497c2539ede136a8ad0c6ab6f4bdf59b28
Michele Ippolito <m.ippolito90@gmail.com>
2024-10-25 17:26:46 +0200

    Fix color bug in PolarSwipe
    Without the = sign, the condition to select a new color was never
    triggering and the effect was only playing one color.

--- andromeda/effects.h
+++ andromeda/effects.h
@@ -416,7 +416,7 @@ class PolarSwipe : public AbstractEffect
       else
         radius = map(v, 0, 65535, scanMin, scanMax);
 
-      if (radius > SCREEN_HALF_SIZE + aperture)
+      if (radius >= SCREEN_HALF_SIZE + aperture)
         color = randomColor();
 
       bandMin = radius - aperture;
