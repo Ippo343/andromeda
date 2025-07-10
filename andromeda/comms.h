0359ae0cc2c779b20ae5e65e5abfc4f71a0640d1
Michele Ippolito <m.ippolito90@gmail.com>
2025-07-10 22:48:02 +0200

    Refactored and improved comms code
    I can't take the credit for this, it was mostly ChatGPT.
    It refactored the request handling code so that it's now sensible and
    not a horrible spaghetti mess, and merged the HTML reply into one string
    that is much faster to send.

--- andromeda/comms.h
+++ andromeda/comms.h
@@ -2,152 +2,169 @@
 #define WEB_SERVER_H
 
 #include <ArduinoLog.h>
-#include <string>
-
-#include "WiFiS3.h"
+#include <WiFiS3.h>
 #include "utils.h"
 #include "mission-control.h"
 #include "secrets.h"
 
+IPAddress local_IP(192, 168, 1, 201);     // Your chosen static IP
+IPAddress gateway(192, 168, 1, 1);        // Your router's IP
+IPAddress subnet(255, 255, 255, 0);       // Usually 255.255.255.0
+IPAddress primaryDNS(8, 8, 8, 8);         // Optional
+IPAddress secondaryDNS(8, 8, 4, 4);       // Optional
+const byte port = 80;
+
 class Comms
 {
-  private:
-    const byte port = 80;
+private:
+
     int status = WL_IDLE_STATUS;
     WiFiServer server;
 
     MissionControl& mc;
 
-  public:
+    static constexpr size_t REQUEST_LINE_BUFFER_SIZE = 64;
+    static constexpr unsigned long REQUEST_TIMEOUT_MS = 100;  // Increased timeout for reliability
+
+public:
 
     Comms(MissionControl& missionControl) :
-      server(port),
-      mc(missionControl)
+        server(port),
+        mc(missionControl)
     {
     }
 
     bool setup()
     {
-      // This doesn't actually work on an R4 :(
-      // That's because the R4 is actually delegating the WiFi to an ESP32,
-      // and the WiFi library does not support setting the hostname from the ESP32 core.
-      // But who knows, if one day support is added, we are ready.
-      WiFi.setHostname("Andromeda");
+        // This doesn't actually work on an R4 :(
+        // That's because the R4 is actually delegating the WiFi to an ESP32,
+        // and the WiFi library does not support setting the hostname from the ESP32 core.
+        // But who knows, if one day support is added, we are ready.
+        WiFi.setHostname("Andromeda");
 
-      status = WiFi.begin(WIFI_SSID, WIFI_PASS);
+        // IMPORTANT: Call WiFi.config() *before* WiFi.begin()
+        WiFi.config(local_IP, primaryDNS, gateway, subnet);
 
-      // Only do one attempt to connect
-      // because everything is synchronous and I don't want it to delay the setup
-      // without any way to indicate what's happening
-      if (status != WL_CONNECTED)
-      {
-        Log.errorln("Could not connect to %s, comms disabled", WIFI_SSID);
-      }
-      else
-      {
-        server.begin();
-        printWifiStatus();
-      }
+        status = WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-      // Return a success flag so that we can at least show the error animation
-      return status == WL_CONNECTED;
+        // Only do one attempt to connect
+        // because everything is synchronous and I don't want it to delay the setup
+        // without any way to indicate what's happening
+        if (status != WL_CONNECTED)
+        {
+            Log.errorln("Could not connect to %s, comms disabled", WIFI_SSID);
+        }
+        else
+        {
+            server.begin();
+            printWifiStatus();
+        }
+
+        // Return a success flag so that we can at least show the error animation
+        return status == WL_CONNECTED;
     }
 
     inline void printWifiStatus()
     {
-      Log.noticeln("Connected to %s with IP %p (%l dBm)", WiFi.SSID(), WiFi.localIP(), WiFi.RSSI());
+        Log.noticeln("Connected to %s with IP %p (%l dBm)", WiFi.SSID(), WiFi.localIP(), WiFi.RSSI());
     }
 
     void loop()
     {
-      if (status != WL_CONNECTED)
-        return;
+        if (status != WL_CONNECTED)
+            return;
 
-      WiFiClient client = server.available();   // listen for incoming clients
+        WiFiClient client = server.available();   // listen for incoming clients
 
-      if (!client)
-        return;
+        if (!client || !client.connected())
+            return;
 
-      // TODO: the code below is taken from the official example, and it sucks.
-      // I tried a few times to refactor it, but failed and I cannot figure out why.
-      // It will do for the moment, but for the love of God this should be much better.
-      // This link has a better tutorial:
-      //    https://werner.rothschopf.net/202003_arduino_webserver_post_en.htm
-      //
-      String currentLine = "";
-      while (client.connected())
-      {
-        if (client.available())
+        char requestLine[REQUEST_LINE_BUFFER_SIZE];
+        if (readRequestLine(client, requestLine, REQUEST_LINE_BUFFER_SIZE))
         {
-          char c = client.read();
-          if (c == '\n')
-          {
-            // if the current line is blank, you got two newline characters in a row.
-            // that's the end of the client HTTP request, so send a response:
-            if (currentLine.length() == 0)
-            {
-              reply(client);
-              break;
-            }
-            else
-            {
-              currentLine = "";
-            }
-          }
-          else if (c != '\r')
-          {
-            // if you got anything else but a carriage return character,
-            // add it to the end of the currentLine
-            currentLine += c;
-          }
-
-          if (currentLine.endsWith("GET /N"))
-          {
-            mc.handleTransition();
-          }
-
-          if (currentLine.endsWith("GET /H"))
-          {
-            mc.holdEffect();
-          }
-
-          if (currentLine.endsWith("GET /D"))
-          {
-            mc.powerOff();
-          }
-
-          if (currentLine.endsWith("GET /U"))
-          {
-            mc.powerOn();
-          }
+            handleRequest(requestLine);
+            reply(client);
         }
-      }
 
-      client.stop();
+        delay(1);  // Allow time for client to receive the response
+        client.stop();
+    }
+
+private:
+    bool readRequestLine(WiFiClient& client, char* buffer, size_t bufferSize)
+    {
+        size_t index = 0;
+        unsigned long start = millis();
+
+        while (client.connected() && millis() - start < REQUEST_TIMEOUT_MS)
+        {
+            if (client.available())
+            {
+                char c = client.read();
+
+                if (c == '\n')
+                {
+                    buffer[index] = '\0';
+                    return true;
+                }
+
+                if (c != '\r' && index < bufferSize - 1)
+                {
+                    buffer[index++] = c;
+                }
+            }
+        }
+
+        buffer[0] = '\0';  // fallback empty string
+        return false;
+    }
+
+    void handleRequest(const char* line)
+    {
+        // Expecting something like: "GET /U HTTP/1.1"
+        if (strncmp(line, "GET /", 5) != 0)
+            return;
+
+        char path = line[5];
+
+        switch (path)
+        {
+            case 'N':
+                mc.handleTransition();
+                break;
+            case 'H':
+                mc.holdEffect();
+                break;
+            case 'D':
+                mc.powerOff();
+                break;
+            case 'U':
+                mc.powerOn();
+                break;
+            default:
+                // Optional: handle unknown path
+                break;
+        }
     }
 
     void reply(WiFiClient& client)
     {
-      milliseconds start = millis();
+        milliseconds start = millis();
 
-      // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-      // and a content-type so the client knows what's coming, then a blank line:
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-type:text/html");
-      client.println();
+        client.println(
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "<body style='background-color:#1a1a1a;'>"
+            "<p style='font-size:7vw;'><a href='/N'>Next</a><br></p>"
+            "<p style='font-size:7vw;'><a href='/H'>Hold</a><br></p>"
+            "<p style='font-size:7vw;'><a href='/D'>Off</a><br></p>"
+            "<p style='font-size:7vw;'><a href='/U'>On</a><br></p>"
+            "</body>"
+        );
 
-      // TODO: way more controls (depends on polishing the control logic)
-      client.print("<body style=\"background-color:1a1a1a;\">");
-      client.print("<p style=\"font-size:7vw;\"><a href=\"/N\">Next</a><br></p>");
-      client.print("<p style=\"font-size:7vw;\"><a href=\"/H\">Hold</a><br></p>");
-      client.print("<p style=\"font-size:7vw;\"><a href=\"/D\">Off</a><br></p>");
-      client.print("<p style=\"font-size:7vw;\"><a href=\"/U\">On</a><br></p>");
-      client.print("</body>");
-
-      // End the HTTP response
-      client.println();
-
-      Log.noticeln("WiFi reply took %d milliseconds", millis() - start);
+        Log.noticeln("WiFi reply took %d milliseconds", millis() - start);
     }
 };
 
